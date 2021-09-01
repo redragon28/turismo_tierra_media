f5f5e8ecac3161849d6591c2f04bbd7895dbde6f
Facundo <facundolhagarats@hotmail.com>
2021-09-01 19:24:22 -0300

    Se agrega clase Atraccion con constructor y getters

--- src/tpTierraMedia/Usuario.java
+++ src/tpTierraMedia/Usuario.java
@@ -1,13 +1,12 @@
 package tpTierraMedia;
 
 public class Usuario {
-	
+
 	private int presupuesto;
 	private int tiempoDisponible;
 	private TipoAtraccion tipoPreferido;
-	
-	public Usuario(int presupuesto, int tiempoDisponible,
-			TipoAtraccion tipoPreferido) {
+
+	public Usuario(int presupuesto, int tiempoDisponible, TipoAtraccion tipoPreferido) {
 		this.presupuesto = presupuesto;
 		this.tiempoDisponible = tiempoDisponible;
 		this.tipoPreferido = tipoPreferido;
