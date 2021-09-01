923ea3eff203c6eca766c117466d97f1155034b9
Facundo <facundolhagarats@hotmail.com>
2021-09-01 19:22:54 -0300

    Se agrega clase Atraccion con constructor y getters

--- src/tpTierraMedia/Atraccion.java
+++ src/tpTierraMedia/Atraccion.java
@@ -2,4 +2,32 @@ package tpTierraMedia;
 
 public class Atraccion {
 
+	private TipoAtraccion tipo;
+	private double precio;
+	private int duracion;
+	private int cupoPersonas;
+
+	public Atraccion(TipoAtraccion tipo, double precio, int duracion, int cupoPersonas) {
+		this.tipo = tipo;
+		this.precio = precio;
+		this.duracion = duracion;
+		this.cupoPersonas = cupoPersonas;
+	}
+
+	public TipoAtraccion getTipo() {
+		return tipo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public int getCupoPersonas() {
+		return cupoPersonas;
+	}
+
 }
