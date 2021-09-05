6717f3817bdf32584ab70c26ebf097840d76dd4e
Emanuel Rossi <emanuel_lp3@hotmail.com>
2021-09-05 12:30:27 -0300

    .

--- src/tpTierraMedia/PromocionAbsoluta.java
+++ src/tpTierraMedia/PromocionAbsoluta.java
@@ -19,14 +19,11 @@ public class PromocionAbsoluta extends Promocion {
 	
 	}
 	
-<<<<<<< HEAD
+
 	public double getPromocion() {
 	return this.valorPaquete;
 }
-=======
-	public double valorPromo() {
-		return 0;
+
 		
 	}
->>>>>>> branch 'main' of https://github.com/redragon28/turismo_tierra_media.git
-}
+
