642da6707f602ba78024bc843867423d31c2d2e2
Alejandro Dolhagarats <ale_facu_mellis@hotmail.com>
2021-09-09 11:17:21 -0300

    Se comienza con la sugerencia de atracciones

--- src/tpTierraMedia/TurismoTierraMedia.java
+++ src/tpTierraMedia/TurismoTierraMedia.java
@@ -1,7 +1,43 @@
 package tpTierraMedia;
 
-public class TurismoTierraMedia {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class TurismoTierraMedia {
+	
+	
+	public static void sugerirAtraccion(Usuario usuario) {
+		
+	}
+
+	
+	
+	
+	
+	
+	//Hay que ver como hacer la comparacion entre TipoPreferido del usuario con el tipoAtraccion
+	public static List<Atraccion> sugerenciasAtraccionesPreferidas(Usuario usuario) {
+		List<Atraccion> atraccionesRecomendadas = new ArrayList<Atraccion>();
+		
+		for (Atraccion atraccion : listaAtracciones) {
+			if ((usuario.getTipoPreferido()).comparteTo(atraccion.getTipo()) == 0) {
+				if (atraccion.getCupoPersonas() > 0 && usuario.getPresupuesto() >= atraccion.getPrecio()
+						&& usuario.getTiempoDisponible() >= atraccion.getDuracion()) {
+					atraccionesRecomendadas.add(atraccion);
+				}
+			}
+		}
+		return atraccionesRecomendadas;
+	}
+	
+	public static void ordenarAtraccionesPorPrecio(List<Atraccion> listaAtracciones) {
+		Collections.sort(listaAtracciones, new PrecioComparator());
+	}
+	
+	public static void ordenarAtraccionesPorDuracion(List<Atraccion> listaAtracciones) {
+		Collections.sort(listaAtracciones, new DuracionComparator());
+	}
 
 
 
