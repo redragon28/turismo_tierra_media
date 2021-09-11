120f14b4faaec4bfdedf4180a5a1fe84e72d8d6b
Facundo <facundolhagarats@hotmail.com>
2021-09-11 17:57:51 -0300

    se crea tipoPromocion()

--- src/tpTierraMedia/PromocionAxB.java
+++ src/tpTierraMedia/PromocionAxB.java
@@ -6,25 +6,29 @@ public class PromocionAxB extends Promocion {
 
 	public PromocionAxB(String nombre, List<Atraccion> atracciones) {
 		super(nombre, atracciones);
-		
-		
+
 	}
+
 	@Override
-public double getPrecio() {
-	double valor = 0;
-	for (int x=0 ; x < atracciones.size()-1; x++) {
-		valor = atracciones.get(x).getPrecio() ;
+	public double getPrecio() {
+		double valor = 0;
+		for (int x = 0; x < atracciones.size() - 1; x++) {
+			valor = atracciones.get(x).getPrecio();
+		}
+		return valor;
 	}
-return valor;
-}
 
-public String VisitaGratis() {
-	return atracciones.get(atracciones.size()).getNombre();
-}
-@Override
-public String toString() {
-	return "PromocionAxB [nombre=" + nombre + ", atracciones=" + atracciones + "]";
-}
+	public String VisitaGratis() {
+		return atracciones.get(atracciones.size()).getNombre();
+	}
 
+	@Override
+	public String toString() {
+		return "PromocionAxB [nombre=" + nombre + ", atracciones=" + atracciones + "]";
+	}
 
+	@Override
+	public int tipoPromocion() {
+		return 1;
+	}
 }
