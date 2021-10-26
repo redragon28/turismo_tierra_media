0c0968f417cf12c029770d0a02a0a7a7d08746e5
Alejandro Dolhagarats <ale_facu_mellis@hotmail.com>
2021-10-26 16:16:16 -0300

    Se crea el conecctionProvider

---
+++ src/db/ConnectionProvider.java
@@ -0,0 +1,22 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+
+	private static Connection connection;
+	
+	public static Connection getConnection() throws SQLException {
+		if (connection == null) {
+			connection = DriverManager.getConnection("jdbc:sqlite:TurismoTierraMedia.db");
+		}
+		
+		return connection;
+	}
+ 
+	//"jdbc:sqlite:TurismoTierraMedia.db" esto depende del nombre que le pongamos al SQL
+}
+
+
